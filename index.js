4ca99ae4c1329c14fa002a14b50b726a518e16ea
Adarsh <137529020+scaramouche18@users.noreply.github.com>
2024-03-04 00:18:36 +0530

    Update index.js
    made it usable with keyboard 👍🏼

--- index.js
+++ index.js
@@ -43,6 +43,40 @@ for(var i = 0; i < numberDrums; i++){
     })
         
 }
+document.addEventListener("keydown", function(event){
+  switch(event.key){
+    case "w":
+      var drumm1 = new Audio("./sounds/tom-1.mp3");
+      drumm1.play();
+      break;
+    case "a":
+      var drum2 = new Audio("./sounds/tom-2.mp3");
+      drum2.play();
+      break;
+    case "s":
+      var drum3 = new Audio("./sounds/tom-3.mp3");
+      drum3.play();
+      break;
+    case "d":
+      var drum4 = new Audio("./sounds/tom-4.mp3");
+      drum4.play();
+      break;
+    case "j":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log("Not a valid key!");
+  }
+});
 
 
 
